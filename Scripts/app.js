62d428ede9b52e5826f1e915d168026652ea143e
Kass-S <kassiespacek101@gmail.com>
2025-01-28 22:14:41 -0800

    started linking elements to app.js

--- Scripts/app.js
+++ Scripts/app.js
@@ -5,6 +5,10 @@ let pkmnRandom = document.getElementById("pkmnRandom");
 
 let pkmnAbilities = document.getElementById("pkmnAbilities");
 let pkmnMoves = document.getElementById("pkmnMoves");
+let pkmnNameNumber = document.getElementById("pkmnNameNumber");
+let pkmnType = document.getElementById("pkmnType");
+let pkmnEvolutionLine = document.getElementById("pkmnEvolutionLine");
+let pkmnLocationText = document.getElementById("pkmnLocationText");
 
 let userSearch = "pikachu";
 let moveList = "";
@@ -13,42 +17,48 @@ let typeList = "";
 //GetPokemon();
 
 pkmnUserSearchBtn.addEventListener('click', async () => {
-    
-})
+    //userSearch = pkmnUserSearch.value;
+    let pkmnData = await GetPokemon(userSearch);
+    console.log(pkmnData.name);
+    console.log(pkmnData.id);
+    pkmnNameNumber.innerText = `${pkmnData.name} - ${pkmnData.id}`;
 
-const GetPokemon = async () => {
-    const promise = await fetch(`https://pokeapi.co/api/v2/pokemon/${userSearch}`);
-    const data = await promise.json();
-    console.log(data.name);
-    console.log(data.id);
+    console.log(pkmnData.sprites.other["official-artwork"].front_default);
+    console.log(pkmnData.sprites.other["official-artwork"].front_shiny);
 
-    console.log(data.sprites.other["official-artwork"].front_default);
-    console.log(data.sprites.other["official-artwork"].front_shiny);
-
-    for(let i = 0; i < data.types.length; i++){
-        typeList += data.types[i].type.name + "\n";
+    for(let i = 0; i < pkmnData.types.length; i++){
+        typeList += pkmnData.types[i].type.name + "\n";
     }
     console.log(typeList);
+    pkmnType.innerText = typeList;
 
-    for(let i = 0; i < data.abilities.length; i++){
-        abilityList += data.abilities[i].ability.name + "\n";
+    for(let i = 0; i < pkmnData.abilities.length; i++){
+        abilityList += pkmnData.abilities[i].ability.name + "\n";
     }
     console.log(abilityList);
+    pkmnAbilities.innerText = abilityList;
 
-    for(let i = 0; i < data.moves.length; i++)
+    for(let i = 0; i < pkmnData.moves.length; i++)
     {
-        moveList += data.moves[i].move.name + ", ";
+        moveList += pkmnData.moves[i].move.name + ", ";
     }
     console.log(moveList);
 
-    let test = await GetLocation(data.id);
-    console.log(test);
+    let pkmnLocation = await GetLocation(pkmnData.id);
+    console.log(pkmnLocation);
+    pkmnLocationText.innerText = pkmnLocation;
+})
+
+const GetPokemon = async (userSearch) => {
+    const promise = await fetch(`https://pokeapi.co/api/v2/pokemon/${userSearch}`);
+    const Data = await promise.json();
+    return Data;
 }
 
 const GetLocation = async (pkmnId) => {
     const promise = await fetch(`https://pokeapi.co/api/v2/pokemon/${pkmnId}/encounters`)
-    const data = await promise.json();
-    console.log(data[0].location_area.name);
-    test2 = data[0].location_area.name;
-    return test2;
+    const Data = await promise.json();
+    console.log(Data[0].location_area.name);
+    let location = Data[0].location_area.name;
+    return location;
 }
