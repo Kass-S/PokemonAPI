61b4559d8c2a578ca88fbc07e246e980ffb88270
Kass-S <kassiespacek101@gmail.com>
2025-01-29 14:25:15 -0800

    finnished evolution and gen limit logic

--- Scripts/app.js
+++ Scripts/app.js
@@ -34,46 +34,81 @@ pkmnUserSearchBtn.addEventListener('click', async () => {
     let moveList = "";
     let abilityList = "";
     let typeList = "";
+    let evoList = "";
 
     userSearch = pkmnUserSearch.value;
+
     let pkmnData = await GetPokemon(userSearch);
-    pkmnNameNumber.innerText = `${pkmnData.name} - ${pkmnData.id}`;
+    console.log(pkmnData)
+    if(pkmnData.id != null){
+        pkmnNameNumber.innerText = `${pkmnData.name} - ${pkmnData.id}`;
 
-    pkmnImage.src = pkmnData.sprites.other["official-artwork"].front_default;    
+        pkmnImage.src = pkmnData.sprites.other["official-artwork"].front_default;    
 
-    for(let i = 0; i < pkmnData.types.length; i++){
-        typeList += pkmnData.types[i].type.name + ", ";
+        for(let i = 0; i < pkmnData.types.length; i++){
+            typeList += pkmnData.types[i].type.name + ", ";
+        }
+        pkmnType.innerText = typeList;
+
+        for(let i = 0; i < pkmnData.abilities.length; i++){
+            abilityList += pkmnData.abilities[i].ability.name + ", ";
+        }
+        pkmnAbilities.innerText = abilityList;
+
+        for(let i = 0; i < pkmnData.moves.length; i++)
+        {
+            moveList += pkmnData.moves[i].move.name + ", ";
+        }
+        pkmnMoves.innerText = moveList;
+
+        let pkmnLocation = await GetLocation(pkmnData.id);
+        pkmnLocationText.innerText = pkmnLocation;
+
+        let pkmonEvoLine = await GetEvolutionLine(pkmnData.id);
+        if(pkmonEvoLine.chain.evolves_to.length > 0){
+
+            for(let i = 0; i < pkmonEvoLine.chain.evolves_to.length; i++){
+
+                evoList += pkmonEvoLine.chain.evolves_to[i].species.name + "-";
+
+                if(pkmonEvoLine.chain.evolves_to[i].evolves_to.length > 0){
+                    for(let j = 0; j < pkmonEvoLine.chain.evolves_to[i].evolves_to.length; j++){
+                        evoList += pkmonEvoLine.chain.evolves_to[i].evolves_to[j].species.name + "-";
+                    }
+                    
+                }
+            }
+            pkmnEvolutionLine.innerText = pkmonEvoLine.chain.species.name + "-" + evoList;
+
+        }else{
+            pkmnEvolutionLine.innerText = "N/A";    
+        }
     }
-    pkmnType.innerText = typeList;
-
-    for(let i = 0; i < pkmnData.abilities.length; i++){
-        abilityList += pkmnData.abilities[i].ability.name + ", ";
-    }
-    pkmnAbilities.innerText = abilityList;
-
-    for(let i = 0; i < pkmnData.moves.length; i++)
-    {
-        moveList += pkmnData.moves[i].move.name + ", ";
-    }
-    pkmnMoves.innerText = moveList;
-
-    let pkmnLocation = await GetLocation(pkmnData.id);
-    pkmnLocationText.innerText = pkmnLocation;
-
-    let pkmonEvoLine = await GetEvolutionLine(pkmnData.id); 
+    
 })
 
 const GetPokemon = async (userSearch) => {
     const promise = await fetch(`https://pokeapi.co/api/v2/pokemon/${userSearch}`);
-    const Data = await promise.json();
-    return Data;
+    if(!promise.ok){
+        pkmnNameNumber.className = "flex justify-center text-2xl mt-5 mx-5";
+        pkmnNameNumber.innerText = "Invalid. Please enter a pokemon from gens 1-5";
+    }else{
+        const data = await promise.json();
+        if(data.id >= 650){
+            pkmnNameNumber.className = "flex justify-center text-2xl mt-5 mx-5";
+            pkmnNameNumber.innerText = "Invalid. Please enter a pokemon from gens 1-5";
+        }
+        pkmnNameNumber.className = "flex justify-center text-3xl mt-10 mx-5";
+        return data;
+    }
+    
 }
 
 const GetLocation = async (pkmnId) => {
     const promise = await fetch(`https://pokeapi.co/api/v2/pokemon/${pkmnId}/encounters`)
-    const Data = await promise.json();
-    console.log(Data[0].location_area.name);
-    let location = Data[0].location_area.name;
+    const data = await promise.json();
+    console.log(data[0].location_area.name);
+    let location = data[0].location_area.name;
     return location;
 }
 
@@ -82,8 +117,10 @@ const GetEvolutionLine = async (pkmnId) => {
     const data = await promise.json();
     console.log(data.evolution_chain.url);
     let evoChainUrl = data.evolution_chain.url;
+
     let evoChain = await GetEvolutionChain(evoChainUrl);
     console.log(evoChain.chain.species.name);
+    return evoChain;
 }
 
 const GetEvolutionChain = async (url) =>{
